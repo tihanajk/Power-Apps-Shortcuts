3d9884896e9477c302626c22b4677400d52b69ba
Tihana Jurković-Kormo <tjkormo@span.eu>
2024-09-17 15:18:15 +0200

    added checking header and field check type

--- locate.js
+++ locate.js
@@ -12,19 +12,26 @@ function loc() {
       return;
     }
 
+    var header = false;
+    Xrm.Page.ui.headerSection.controls.get().forEach((c) => {
+      var attr = c?.getAttribute();
+      var name = attr?.getName();
+
+      if (name == field) header = true;
+    });
+
     var tab = "";
     var section = "";
+
+    var supportedTypes = ["standard", "lookup", "choices", "choice"];
     Xrm.Page.ui.tabs.get().forEach((t) =>
       t.sections.get().forEach((s) =>
         s.controls.get().forEach((c) => {
-          var name = "";
-          try {
-            var attr = c?.getAttribute();
-            name = attr?.getName();
-          } catch (e) {
-            console.log(e);
-            return;
-          }
+          var type = c?.getControlType();
+          if (supportedTypes.indexOf(type) == -1) return;
+
+          var attr = c?.getAttribute();
+          var name = attr?.getName();
 
           if (name == field) {
             if (tab != "") tab += ", ";
@@ -36,8 +43,13 @@ function loc() {
       )
     );
 
+    var message = header ? "Field found in header" : "";
     if (tab != "" && section != "") {
-      alert(`Tab: ${tab}\nSection: ${section}`);
+      if (header) message += "\n";
+      message += `Tab: ${tab}\nSection: ${section}`;
+    }
+    if (message != "") {
+      alert(message);
     } else {
       alert("Field not found");
     }
